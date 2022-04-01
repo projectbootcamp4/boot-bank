c7f7dd4356a1d978b4ea0458ec7350288eec8ca2
csomervil <carson.somerville@mail.utoronto.ca>
2022-04-01 19:42:22 -0400

    finished input check feature and added refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,28 @@
 botButton = document.getElementById("bot-btn").addEventListener("click", getToken);
-
+/* TODO: ADD VALIDATION AND RESPECTIVE ALERT FOR MESSAGE BOX */
 
 function getToken() {
 var input = document.getElementById("bot-message").value
-console.log(input)
 
-/* need get email from login page:*/
+
+/* need to get email from login page:*/
 var email = document.getElementById("bot-email").value
 
 /* front slash means start and end of expression, back slash is an escape character (similar to new line), + joins the strings, S is whitespace */
 
 /* the variable that is being declared states that an email should be: letters or numbers + @ + numbers or letters + . + number or leters */
 var valid = /\S+@\S+\.\S+/;
-        if (valid.test(email) === false) {
-            alert("invalid email")
-            return false
-        }
+if (valid.test(email) === false) {
+  alert("Invalid Email")
+  return false
+  
+} else if (input === "") {
+  alert("Message Required")
+  return false
+}
+
 
 url = "https://api.telegram.org/bot5233658091:AAEdJMSfyzxehFe65PADr1c2zGFlf5TRvIg/sendMessage?chat_id=5218071898&text=" + email + " : " + input;
-    
 fetch(url) 
 .then(function (response) {
   return response.json();
@@ -27,5 +31,6 @@ fetch(url)
   console.log(data);
 
 });
+window.location.reload();
 };
 
