46a4b863a937bde1c6cbdd0bf5db5be884f7b71e
csomervil <carson.somerville@mail.utoronto.ca>
2022-04-01 15:36:10 -0400

    added email check, and changed layout for bot message display

--- bot.js
+++ bot.js
@@ -1,9 +1,23 @@
 botButton = document.getElementById("bot-btn").addEventListener("click", getToken);
 
+
 function getToken() {
 var input = document.getElementById("bot-message").value
 console.log(input)
-url = "https://api.telegram.org/bot5233658091:AAEdJMSfyzxehFe65PADr1c2zGFlf5TRvIg/sendMessage?chat_id=5218071898&text=" + input;
+
+/* need get email from login page:*/
+var email = document.getElementById("bot-email").value
+
+/* front slash means start and end of expression, back slash is an escape character (similar to new line), + joins the strings, S is whitespace */
+
+/* the variable that is being declared states that an email should be: letters or numbers + @ + numbers or letters + . + number or leters */
+var valid = /\S+@\S+\.\S+/;
+        if (valid.test(email) === false) {
+            alert("invalid email")
+            return false
+        }
+
+url = "https://api.telegram.org/bot5233658091:AAEdJMSfyzxehFe65PADr1c2zGFlf5TRvIg/sendMessage?chat_id=5218071898&text=" + email + " : " + input;
     
 fetch(url) 
 .then(function (response) {
