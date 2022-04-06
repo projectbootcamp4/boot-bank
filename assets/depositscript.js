fecbdb8eb00e59fadf222c63df3694277734b1a8
David Sklar <97452998+davidhsklar@users.noreply.github.com>
2022-04-06 16:22:09 -0400

    Update depositscript.js

--- assets/depositscript.js
+++ assets/depositscript.js
@@ -21,6 +21,7 @@ var userWithdraw = document.getElementById("userWithdraw");
  var totalWithdrawal = document.getElementById("totalWithdrawal");
 
  var currentDate = new Date().toLocaleDateString();
+date.innerText = currentDate();
 // Create our number formatter.
 const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
